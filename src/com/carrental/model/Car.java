9badb887164e9a5e5673a2b1e4fb67e4cef92d9a
Ali Salama <alisalamadev@gmail.com>
2026-01-28 04:00:39 +0200

    add feature/car branch and implement car class

--- src/com/carrental/model/Car.java
+++ src/com/carrental/model/Car.java
@@ -1,4 +1,47 @@
 package com.carrental.model;
 
 public class Car {
+    private String carId;
+    private String brand;
+    private String model;
+    private double pricePerDay;
+    private boolean isAvailable;
+
+    Car(String carId, String brand, String model, double pricePerDay){
+        this.carId = carId;
+        this.brand = brand;
+        this.model = model;
+        this.pricePerDay = pricePerDay;
+        this.isAvailable = true;
+    }
+
+    //get method to get all private var;
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void rent(){
+        isAvailable = false;
+    }
+
+    public void returnCar(){
+        isAvailable = true;
+    }
 }
